479d77a580f9e580b54301cca2441ca1d70b7033
idiom-bytes <idiom.bytes@gmail.com>
2023-07-13 16:53:47 -0700

    Leaving login button so I can still test open ai.

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@/auth'
-// import { LoginButton } from '@/components/login-button'
+import { LoginButton } from '@/components/login-button'
 import { redirect } from 'next/navigation'
 
 export default async function SignInPage() {
@@ -10,6 +10,7 @@ export default async function SignInPage() {
   }
   return (
     <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
+      <LoginButton />
       <p>MetaMask auth to login</p>
     </div>
   )

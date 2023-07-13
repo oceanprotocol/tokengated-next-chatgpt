ae54a19e2ea004cfed871cbc812357d0233bcbaa
idiom-bytes <idiom.bytes@gmail.com>
2023-07-12 21:55:35 -0700

    Trying to get tanstack/react-query to work with vercel

--- components/providers.tsx
+++ components/providers.tsx
@@ -7,17 +7,26 @@ import { ThemeProviderProps } from 'next-themes/dist/types'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { metamaskWallet, ThirdwebProvider } from "@thirdweb-dev/react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
 export function Providers({ children, ...props }: ThemeProviderProps) {
+  const [client] = useState(new QueryClient());
+
   return (
     <NextThemesProvider {...props}>
       <TooltipProvider>
-        <ThirdwebProvider 
-          supportedWallets={[
-            metamaskWallet()
-          ]}
-          activeChain="ethereum">
-          {children}
-        </ThirdwebProvider>
+        <QueryClientProvider client={client}>
+          <ThirdwebProvider 
+            supportedWallets={[
+              metamaskWallet()
+            ]}
+            activeChain="ethereum">
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ThirdwebProvider>
+        </QueryClientProvider>
       </TooltipProvider>
     </NextThemesProvider>
   )

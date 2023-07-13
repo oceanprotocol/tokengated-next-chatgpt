d8878b41045ebb4dc382768cf5fbea02b300ea2c
idiom-bytes <idiom.bytes@gmail.com>
2023-07-12 22:04:32 -0700

    Trying to fix vercel build

--- components/providers.tsx
+++ components/providers.tsx
@@ -7,24 +7,21 @@ import { ThemeProviderProps } from 'next-themes/dist/types'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { metamaskWallet, ThirdwebProvider } from "@thirdweb-dev/react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
-  const [client] = useState(new QueryClient());
-
   return (
     <NextThemesProvider {...props}>
       <TooltipProvider>
-        <QueryClientProvider client={client}>
+      <QueryClientProvider client={queryClient}>
           <ThirdwebProvider 
             supportedWallets={[
               metamaskWallet()
             ]}
             activeChain="ethereum">
             {children}
-            <ReactQueryDevtools initialIsOpen={false} />
           </ThirdwebProvider>
         </QueryClientProvider>
       </TooltipProvider>

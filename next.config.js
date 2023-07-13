1900f0e750bb2d33bd8ff2debefc4acb681d0501
idiom-bytes <idiom.bytes@gmail.com>
2023-07-13 16:40:30 -0700

    solving wagmi weback requirements.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ module.exports = {
     serverActions: true,
     appDir: true
   },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
   images: {
     remotePatterns: [
       {

0ef0da82b5a260d89dee9f30d552c8d265c12ea5
idiom-bytes <idiom.bytes@gmail.com>
2023-07-16 17:51:35 -0700

    User is now routed to the chat

--- auth.ts
+++ auth.ts
@@ -21,62 +21,10 @@ export const auth = async () => {
     if (user) {
       return user
     }
+  } else {
+    if(address) cookies().delete('address')
+    if(web3jwt) cookies().delete('web3jwt')
   }
 
-  cookies().delete('address')
-  cookies().delete('web3jwt')
-  throw new Error('An unexpected error occurred')
-  // return jsonResponse(401, { error: { message: 'Your token has expired.' } })
-
-  // // Create a Supabase client configured to use cookies
-  // let supabase = createMiddlewareClient({ req, res })
-
-  // // Refresh session if expired - required for Server Components
-  // const {
-  //   data: { session }
-  // } = await supabase.auth.getSession()
-
-  // // TODO - sign in via user w/ jwt to provide a session
-  // // Official Implementation: https://github.com/orgs/supabase/discussions/1849
-  // // 1. get jwt
-  // // 2. check if jwt is valid
-  // // 3. if !session but jwt is valid, create session
-  // console.log("req.cookies: ", req.cookies.getAll())
-
-  // const address = req.cookies.get('address')
-  // const web3jwt = req.cookies.get('web3jwt')
-  // if (!session && web3jwt && address) {
-  //   console.log("address cookie:", address)
-  //   console.log("web3jwt cookie:", web3jwt)
-    
-  //   const url = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-  //   const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-  //   const headers = { global: { headers: { Authorization: `Bearer ${web3jwt}` }}}
-  //   console.log("create new supabase client...")
-    
-  //   supabase = createClient(url, anonKey, headers)
-
-  //   console.log("supabase client:", supabase)
-  //   console.log("supabase.auth:", supabase.auth)
-  //   console.log("supabase.auth.getSession:", await supabase.auth.getSession())
-  // }
-
-  // // OPTIONAL: this forces users to be logged in to use the chatbot.
-  // // If you want to allow anonymous users, simply remove the check below.
-  // if (!session && !req.url.includes('/sign-in')) {
-  //   const redirectUrl = req.nextUrl.clone()
-  //   redirectUrl.pathname = '/sign-in'
-  //   redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
-  //   return NextResponse.redirect(redirectUrl)
-  // }
-
-
-
-  // Create a Supabase client configured to use cookies
-  // const supabase = createServerActionClient({ cookies })
-  // const { data, error } = await supabase.auth.getSession()
-  // if (error) throw error
-  // return data.session
-
-  // throw new Error('An unexpected error occurred')
+  return null
 }
--- components/login-button-metamask.tsx
+++ components/login-button-metamask.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import * as React from 'react'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useState, useEffect } from 'react'
 
 import { cn } from '@/lib/utils'
 import { Button, type ButtonProps } from '@/components/ui/button'
 import { IconMetamask, IconSpinner } from '@/components/ui/icons'
 
 import { useRouter } from 'next/navigation';
-import { auth } from '@/auth'
 
 import { 
-  useAccount, 
-  useNetwork
+  useAccount
 } from "wagmi";
 
 import { signMessage } from '@wagmi/core'
@@ -28,12 +25,19 @@ export function LoginButtonMetamask({
   className,
   ...props
 }: LoginButtonProps) {
-  const [isLoading, setIsLoading] = React.useState(false)
-
-  const supabase = createClientComponentClient()
+  const [isLoading, setIsLoading] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const router = useRouter()
+  
   const { address, isConnected } = useAccount();
   // const { chains } = useNetwork();
   
+  useEffect(() => {
+    if( isAuthenticated ) {
+      router.push('/');
+    }
+  }, [isAuthenticated])
+
   return (
     <Button
       variant="outline"
@@ -41,51 +45,55 @@ export function LoginButtonMetamask({
         setIsLoading(true)
         const userAddress = address ? address : '';
 
-        try {
-          // 1. Get a nonce from the server
-          const nonceResponse = await fetch(`/api/web3auth/nonce`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              address: userAddress,
-            }),
-          });
+        if(userAddress && isConnected) {
+          try {
+            // 1. Get a nonce from the server
+            const nonceResponse = await fetch(`/api/web3auth/nonce`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                address: userAddress,
+              }),
+            });
 
-          const { user: [user] } = await nonceResponse.json();
-          
-          // 2. Ask the user to sign a message
-          const message = process.env.NEXT_PUBLIC_WEB3AUTH_MESSAGE + user.auth.genNonce;
-          const signedMessage = await signMessage({
-            message: message,
-          })
-
-          // // 3. Send the signed message to our API
-          const response = await fetch(`/api/web3auth/login`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              address: userAddress,
-              signedMessage: signedMessage,
-              nonce: user.auth.genNonce
+            const { user: [user] } = await nonceResponse.json();
+            
+            // 2. Ask the user to sign a message
+            const message = process.env.NEXT_PUBLIC_WEB3AUTH_MESSAGE + user.auth.genNonce;
+            const signedMessage = await signMessage({
+              message: message,
             })
-          })
-      
-          // Handle the response from the API
-          if (response.ok) {
-            // The verification was successful
-            console.log('Verification successful!')
-          } else {
-            // The verification failed
-            console.error('Verification failed!');
+
+            // // 3. Send the signed message to our API
+            const response = await fetch(`/api/web3auth/login`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                address: userAddress,
+                signedMessage: signedMessage,
+                nonce: user.auth.genNonce
+              })
+            })
+        
+            // Handle the response from the API
+            if (response.ok) {
+              // The verification was successful
+              console.log('Verification successful!')
+              setIsAuthenticated(true)
+            } else {
+              // The verification failed
+              console.error('Verification failed!');
+              setIsAuthenticated(false)
+            }
+          } catch (err) {
+            console.error('An error occurred:', err);
+          } finally {
+            setIsLoading(false);
           }
-        } catch (err) {
-          console.error('An error occurred:', err);
-        } finally {
-          setIsLoading(false);
         }
       }}      
       disabled={isLoading}
